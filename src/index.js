445792d57aa6ae9e89891fc87881285bf1052ef9
Diarmuid O Connor <doconnor@wit.ie>
2022-08-30 09:20:11 +0100

    Enabled Favourites and Full review pages

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import {createRoot} from "react-dom/client";
 import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
-// import FavouriteMoviesPage from "./pages/favouriteMoviesPage"; // NEW
-// import MovieReviewPage from "./pages/movieReviewPage";
+import FavouriteMoviesPage from "./pages/favouriteMoviesPage"; // NEW
+import MovieReviewPage from "./pages/movieReviewPage";
 import SiteHeader from "./components/siteHeader";
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
@@ -39,12 +39,12 @@ const App = () => {
         <MoviesContextProvider>
           <Routes>
           <Route path="/movies/:id" element={<MoviePage />} /> 
-          {/* <Route path="/reviews/form" element={<AddMovieReviewPage/>} />
+           {/* <Route path="/reviews/form" element={<AddMovieReviewPage/>} /> */}
             <Route path="/reviews/:id" element={<MovieReviewPage />} />
             <Route
               path="/movies/favourites"
               element={<FavouriteMoviesPage />}
-            />  */}
+            /> 
             <Route path="/" element={<HomePage />} />
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
